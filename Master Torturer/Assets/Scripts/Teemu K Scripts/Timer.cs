d02111b24d700b71896ad64f4354a9224f715ddf
Taistonalle <55252093+Taistonalle@users.noreply.github.com>
2023-01-21 21:16:18 +0200

    Timer base and one addition to .gitignore
    Made a semi basic timer that can be easily modified later.

---
+++ Master Torturer/Assets/Scripts/Teemu K Scripts/Timer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+    //This can be modifed in the unity inspector
+    [Tooltip("Time for the timer. Press T to activate")][SerializeField] float time;
+
+    bool timerOn;
+
+    void Update() {
+        //Test button
+        if (Input.GetKey(KeyCode.T) && !timerOn) {
+            StartCoroutine(CoroutineTimer());
+        }
+    }
+
+    IEnumerator CoroutineTimer() {
+        timerOn = true;
+        Debug.Log("Timer started");
+        yield return new WaitForSeconds(time);
+        Debug.Log("Time ran out! Do something here.");
+        timerOn = false;
+    }
+}
