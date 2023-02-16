6e4b39ae4d2776ee34e26db59aa34ee5d798bccb
Taistonalle <55252093+Taistonalle@users.noreply.github.com>
2023-02-16 20:02:11 +0200

    Puzzle D Remade
    Remade Puzzle D to a certain point after a major F. No valves involved yet. Maybe later

---
+++ Master Torturer/Assets/Scripts/Teemu K Scripts/PuzzleD.cs
@@ -0,0 +1,129 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PuzzleD : MonoBehaviour {
+    PuzzleState puzzleState;
+    public Vector3[] gageStartPos, cageHeightlevels;
+    [SerializeField] GameObject[] cages;
+    [SerializeField] PuzzleDCage[] cageScripts;
+    [Tooltip("Value wanted for the distance between levels")][SerializeField] float cageLevelDistance;
+
+    void Start() {
+        puzzleState = PuzzleState.Unfinished;
+
+        //Find all cages from the scene
+        cages = GameObject.FindGameObjectsWithTag("Cage");
+
+        //Create new empty array with amount of levels wanted
+        cageHeightlevels = new Vector3[4];
+
+        //Same for cage scripts
+        cageScripts = new PuzzleDCage[3];
+
+        //Set cage height level heights
+        cageHeightlevels[0].y = cages[0].transform.position.y;
+        cageHeightlevels[1].y = cageHeightlevels[0].y - cageLevelDistance;
+        cageHeightlevels[2].y = cageHeightlevels[1].y - cageLevelDistance;
+        cageHeightlevels[3].y = cageHeightlevels[2].y - cageLevelDistance;
+
+        //Set scripts to their array
+        cageScripts[0] = cages[0].GetComponent<PuzzleDCage>();
+        cageScripts[1] = cages[1].GetComponent<PuzzleDCage>();
+        cageScripts[2] = cages[2].GetComponent<PuzzleDCage>();
+    }
+
+    void Update() {
+        //Debug test buttons "Vasen vastapäivä" simulation
+        if (Input.GetKeyDown(KeyCode.K) && puzzleState != PuzzleState.Finished) {
+            //kutsu cagen script kautta meno suuntaa yms
+            cageScripts[2].StartCoroutine("MoveCageDown");
+            cageScripts[1].StartCoroutine("MoveCageUp");
+        }
+        //"Vasen myötäpäivä" simulation
+        else if (Input.GetKeyDown(KeyCode.L) && puzzleState != PuzzleState.Finished) {
+            cageScripts[1].StartCoroutine("MoveCageDown");
+            cageScripts[0].StartCoroutine("MoveCageUp");
+        }
+        //"Oikea vastapäivä"
+        else if (Input.GetKeyDown(KeyCode.O) && puzzleState != PuzzleState.Finished) {
+            cageScripts[0].StartCoroutine("MoveCageDown");
+        }
+        //"Oikea myötäpäivä"
+        else if (Input.GetKeyDown(KeyCode.P) && puzzleState != PuzzleState.Finished) {
+            cageScripts[2].StartCoroutine("MoveCageUp");
+        }
+
+        //Check if cages are in correct spots to finish puzzle
+        if(puzzleState != PuzzleState.Finished && cages[0].transform.position.y == cageHeightlevels[3].y && cages[1].transform.position.y == cageHeightlevels[2].y
+            && cages[2].transform.position.y == cageHeightlevels[1].y) {
+            Debug.Log("Puzzle is correct!");
+            puzzleState = PuzzleState.Finished;
+        }
+
+    }
+
+    //Not like this
+    /*
+    void MoveCageDown(GameObject cage, Vector3 direction) {
+        //Downwards movement stuff
+        if (cage.GetComponent<PuzzleDCage>().cageState == CageState.ZeroLevel && cage.transform.position.y > cageHeightlevels[1].y) {
+            cage.transform.position += cageMoveSpeed * Time.deltaTime * direction;
+            if (cage.transform.position.y < cageHeightlevels[1].y) {
+                cageMoving3 = false;
+                cage.GetComponent<PuzzleDCage>().cageState = CageState.FirstLevel;
+                cage.transform.position = new Vector3(cage.transform.position.x, cageHeightlevels[1].y);
+            }
+        }
+        else if (cage.GetComponent<PuzzleDCage>().cageState == CageState.FirstLevel && cage.transform.position.y > cageHeightlevels[2].y) {
+            cage.transform.position += cageMoveSpeed * Time.deltaTime * direction;
+            if (cage.transform.position.y < cageHeightlevels[2].y) {
+                cageMoving3 = false;
+                cage.GetComponent<PuzzleDCage>().cageState = CageState.SecondLevel;
+                cage.transform.position = new Vector3(cage.transform.position.x, cageHeightlevels[2].y);
+
+            }
+        }
+        else if (cage.GetComponent<PuzzleDCage>().cageState == CageState.SecondLevel && cage.transform.position.y >= cageHeightlevels[3].y) {
+            cage.transform.position += cageMoveSpeed * Time.deltaTime * direction;
+            if (cage.transform.position.y <= cageHeightlevels[3].y || cage.transform.position.y == cageHeightlevels[3].y) {
+                cageMoving3 = false;
+                cage.GetComponent<PuzzleDCage>().cageState = CageState.ThirdLevel;
+                cage.transform.position = new Vector3(cage.transform.position.x, cageHeightlevels[3].y);
+            }
+        }
+        //else if(cage.GetComponent<PuzzleDCage>().cageState == CageState.ThirdLevel && cage.transform.position)
+    }
+
+    void MoveCageUp(GameObject cage, Vector3 direction) {
+        //Upwards movement stuff
+        if (cage.GetComponent<PuzzleDCage>().cageState == CageState.ThirdLevel && cage.transform.position.y < cageHeightlevels[2].y) {
+            cage.transform.position += cageMoveSpeed * Time.deltaTime * direction;
+            if (cage.transform.position.y > cageHeightlevels[2].y) {
+                cageMoving2 = false;
+                cage.GetComponent<PuzzleDCage>().cageState = CageState.SecondLevel;
+                cage.transform.position = new Vector3(cage.transform.position.x, cageHeightlevels[2].y);
+            }
+        }
+        else if (cage.GetComponent<PuzzleDCage>().cageState == CageState.SecondLevel && cage.transform.position.y < cageHeightlevels[1].y) {
+            cage.transform.position += cageMoveSpeed * Time.deltaTime * direction;
+            if (cage.transform.position.y > cageHeightlevels[1].y) {
+                cageMoving2 = false;
+                cage.GetComponent<PuzzleDCage>().cageState = CageState.FirstLevel;
+                cage.transform.position = new Vector3(cage.transform.position.x, cageHeightlevels[1].y);
+
+            }
+        }
+        else if (cage.GetComponent<PuzzleDCage>().cageState == CageState.FirstLevel && cage.transform.position.y <= cageHeightlevels[0].y) {
+            cage.transform.position += cageMoveSpeed * Time.deltaTime * direction;
+            if (cage.transform.position.y >= cageHeightlevels[0].y || cage.transform.position.y == cageHeightlevels[0].y) {
+                cageMoving2 = false;
+                cage.GetComponent<PuzzleDCage>().cageState = CageState.ZeroLevel;
+                cage.transform.position = new Vector3(cage.transform.position.x, cageHeightlevels[0].y);
+            }
+        }
+    }
+    */
+}
---
+++ Master Torturer/Assets/Scripts/Teemu K Scripts/PuzzleDCage.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CageState { ZeroLevel, FirstLevel, SecondLevel, ThirdLevel }
+public class PuzzleDCage : MonoBehaviour {
+    public CageState cageState;
+    PuzzleD puzzleD;
+    [SerializeField] float cageMoveSpeed;
+
+    void Start() {
+        cageState = CageState.ZeroLevel;
+        puzzleD = FindObjectOfType<PuzzleD>();
+    }
+
+    void MoveCage(Vector3 direction) {
+        transform.position += cageMoveSpeed * Time.deltaTime * direction;
+    }
+    
+    public IEnumerator MoveCageDown() { 
+        while (cageState == CageState.ZeroLevel && transform.position.y > puzzleD.cageHeightlevels[1].y) {
+            yield return new WaitForSeconds(Time.deltaTime);
+            MoveCage(Vector3.down);
+        }
+        while (cageState == CageState.FirstLevel && transform.position.y > puzzleD.cageHeightlevels[2].y) {
+            yield return new WaitForSeconds(Time.deltaTime);
+            MoveCage(Vector3.down);
+        }
+        while (cageState == CageState.SecondLevel && transform.position.y > puzzleD.cageHeightlevels[3].y) {
+            yield return new WaitForSeconds(Time.deltaTime);
+            MoveCage(Vector3.down);
+        }
+        CheckLevel1();
+        //Debug.Log("Olet t‰‰ll‰ 1");
+    }
+    public IEnumerator MoveCageUp() {
+        while (cageState == CageState.ThirdLevel && transform.position.y < puzzleD.cageHeightlevels[2].y) {
+            yield return new WaitForSeconds(Time.deltaTime);
+            MoveCage(Vector3.up);
+        }
+        while (cageState == CageState.SecondLevel && transform.position.y < puzzleD.cageHeightlevels[1].y) {
+            yield return new WaitForSeconds(Time.deltaTime);
+            MoveCage(Vector3.up);
+        }
+        while (cageState == CageState.FirstLevel && transform.position.y < puzzleD.cageHeightlevels[0].y) {
+            yield return new WaitForSeconds(Time.deltaTime);
+            MoveCage(Vector3.up);
+        }
+        CheckLevel2();
+        //Debug.Log("Olet t‰‰ll‰ 2");
+    }
+
+    void CheckLevel1() {
+        //Check if where cage is and update its state to correct one
+        if(cageState == CageState.ZeroLevel && transform.position.y < puzzleD.cageHeightlevels[1].y) {
+            transform.position = new Vector3(transform.position.x, puzzleD.cageHeightlevels[1].y);
+            cageState = CageState.FirstLevel;
+        }
+        else if (cageState == CageState.FirstLevel && transform.position.y < puzzleD.cageHeightlevels[2].y) {
+            transform.position = new Vector3(transform.position.x, puzzleD.cageHeightlevels[2].y);
+            cageState = CageState.SecondLevel;
+        }
+        else if (cageState == CageState.SecondLevel && transform.position.y < puzzleD.cageHeightlevels[3].y) {
+            transform.position = new Vector3(transform.position.x, puzzleD.cageHeightlevels[3].y);
+            cageState = CageState.ThirdLevel;
+        }
+    }
+    void CheckLevel2() {
+        //Check if where cage is and update its state to correct one
+        if (cageState == CageState.ThirdLevel && transform.position.y > puzzleD.cageHeightlevels[2].y) {
+            transform.position = new Vector3(transform.position.x, puzzleD.cageHeightlevels[2].y);
+            cageState = CageState.SecondLevel;
+        }
+        else if (cageState == CageState.SecondLevel && transform.position.y > puzzleD.cageHeightlevels[1].y) {
+            transform.position = new Vector3(transform.position.x, puzzleD.cageHeightlevels[1].y);
+            cageState = CageState.FirstLevel;
+        }
+        else if (cageState == CageState.FirstLevel && transform.position.y > puzzleD.cageHeightlevels[0].y) {
+            transform.position = new Vector3(transform.position.x, puzzleD.cageHeightlevels[0].y);
+            cageState = CageState.ZeroLevel;
+        }
+    }
+}
