b3e1eaf34b582cf2d9c3ec299b41b96a7436ec4d
JENOVA\teemu <teemu.homi@netikka.fi>
2023-03-01 17:34:28 +0200

    Polished Jumspscare and camera! New sceam sound and deadmanx.

--- Master Torturer/Assets/Scripts/Teemu H Scripts/Jumpscare.cs
+++ Master Torturer/Assets/Scripts/Teemu H Scripts/Jumpscare.cs
@@ -20,27 +20,30 @@ public class Jumpscare : MonoBehaviour
             if (puzzleA.puzzleAFinished && puzzleA.leftValveInstantiated)
             {
                 Debug.Log("JUMPSCARE ENABLED!");
+                AudioManager.Instance.Play("MonsterRoarGrowl024");
+                Vector3 deadmanxPosition = new Vector3(-6.476f, 0.08f, 2.29f);
+                Quaternion deadmanxRotation = Quaternion.Euler(0f, 114.1f, 0f); // create a new Quaternion with the desired rotation
                 AudioManager.Instance.Play("Slam10");
-                Vector3 deadmanxPosition = new Vector3(-6.098f, 0.11f, 2.215924f);
-                Quaternion deadmanxRotation = Quaternion.Euler(0.0274106748f, 124.526f, 0.00338907214f); // create a new Quaternion with the desired rotation
                 GameObject newdeadmanx = Instantiate(deadmanx, deadmanxPosition, deadmanxRotation); // instantiate the crawler object with the desired position and rotation
 
                 virtualJumpscareCamera.Priority = 13;
-                StartCoroutine(CameraPriority14());
+                //StartCoroutine(CameraPriority14());
                 jumpscareTriggered = true;
+                StartCoroutine(ResetCameraPriorityAfterDelay());
+
             }
         }
     }
 
-    private IEnumerator CameraPriority14() {
-        yield return new WaitForSeconds(4f);
-        virtualJumpscareFallCamera.Priority = 14;
-        StartCoroutine(ResetCameraPriorityAfterDelay());
-    }
+    //private IEnumerator CameraPriority14() {
+    //    yield return new WaitForSeconds(4f);
+    //    virtualJumpscareFallCamera.Priority = 14;
+    //    StartCoroutine(ResetCameraPriorityAfterDelay());
+    //}
 
     private IEnumerator ResetCameraPriorityAfterDelay()
     {
-        yield return new WaitForSeconds(4f);
+        yield return new WaitForSeconds(8f);
         virtualJumpscareCamera.Priority = 1;
         virtualJumpscareFallCamera.Priority = 1;
     }
